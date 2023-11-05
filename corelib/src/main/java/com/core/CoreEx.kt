8b574cdaf0c97a0341d0a66b57e89438a2fd651c
xyz <xyz@gmail.com>
2023-11-05 08:53:57 +0800

    2.1.1

--- corelib/src/main/java/com/core/CoreEx.kt
+++ corelib/src/main/java/com/core/CoreEx.kt
@@ -1,7 +1,9 @@
 package com.core
 
 import android.content.Context
+import android.content.res.Resources.Theme
 import android.graphics.Color
+import android.graphics.drawable.Drawable
 import androidx.core.content.ContextCompat
 
 /**
@@ -20,4 +22,17 @@ fun Float.sp(context: Context): Float = this / context.resources.displayMetrics.
 
 fun String.color(): Int = Color.parseColor(this)
 
-fun Int.color(context: Context): Int = ContextCompat.getColor(context, this)
+fun Int.res2color(context: Context): Int = ContextCompat.getColor(context, this)
+
+fun Int.res2str(context: Context): String = context.resources.getString(this)
+
+fun Int.res2int(context: Context): Int = context.resources.getInteger(this)
+
+fun Int.res2intArr(context: Context): IntArray = context.resources.getIntArray(this)
+
+fun Int.res2bool(context: Context): Boolean = context.resources.getBoolean(this)
+
+fun Int.res2dimens(context: Context): Float = context.resources.getDimension(this)
+
+fun Int.res2drawable(context: Context, theme: Theme? = null): Drawable =
+    context.resources.getDrawable(this, theme)
