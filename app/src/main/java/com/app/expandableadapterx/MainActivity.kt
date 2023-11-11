1535a5348ccbc391a237e152303add4e7d893bfa
xyz <xyz@gmail.com>
2023-11-11 11:03:20 +0800

    2.1.3

--- app/src/main/java/com/app/expandableadapterx/MainActivity.kt
+++ app/src/main/java/com/app/expandableadapterx/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.ui.AppBarConfiguration
 import com.app.expandableadapterx.databinding.ActivityMainBinding
+import com.core.encryption.AESUtil
 import com.google.gson.reflect.TypeToken
 import com.core.log.Log
 import com.core.log.Log.Companion.log
@@ -28,12 +29,21 @@ class MainActivity : AppCompatActivity() {
         log.i(Util.replaceChar("1234567", 0, 2, 10, "*"))
         log.i(Util.replaceChar(null, 0, 2, 10, "*"))
 
-        val type = object :TypeToken<TestModel>(){}.type
+        val type = object : TypeToken<TestModel>() {}.type
         testHttp()
+        testCrypt()
     }
 
 
     private fun testHttp() {
 
+    }
+
+    private fun testCrypt() {
+        val pwd = "1234567890123456"
+        val e = AESUtil.encryptBase64(pwd, "31b6d9135b8053b367e72f03439e4e0757e8ffd8a06b3f7f57f02e1e35e6c889")
+        log.e(e)
+        val d = AESUtil.decryptBase64(pwd, e)
+        log.e(d)
     }
 }
--- corelib/src/main/java/com/core/encryption/AESUtil.kt
+++ corelib/src/main/java/com/core/encryption/AESUtil.kt
@@ -15,21 +15,7 @@ import javax.crypto.spec.SecretKeySpec
  * @IvParameterSpec必须16个字节长度 默认使用128位进行加密和解密
  */
 object AESUtil {
-    private fun convertSecretLen(secretLen: SecretLen): Int {
-        return when (secretLen) {
-            SecretLen.LEN_BIT_192 -> 192
-            SecretLen.LEN_BIT_256 -> 256
-            else -> 128
-        }
-    }
 
-    /**
-     * 默认获取字节采用的utf-8编码
-     *
-     * @param secretSeed 密钥
-     * @param content    要加密的内容
-     * @return 加密之后采用base64转码
-     */
     fun encryptBase64(secretSeed: String, content: String): String {
         var sen = ""
         try {
@@ -51,13 +37,6 @@ object AESUtil {
         return sen
     }
 
-    /**
-     * 默认获取字节采用的utf-8编码
-     *
-     * @param secretSeed    密钥
-     * @param contentBase64 要解密经过base64转码的字符串
-     * @return 解密之后的内容
-     */
     fun decryptBase64(secretSeed: String, contentBase64: String): String {
         var den = ""
         try {
@@ -78,73 +57,17 @@ object AESUtil {
         return den
     }
 
-    /**
-     * 参数以及返回值都是纯字节
-     *
-     * @param secretSeed 密钥
-     * @param content    要加密的字节
-     * @return 加密之后的字节
-     */
     fun encrypt(secretSeed: ByteArray, content: ByteArray?): ByteArray? {
-        return work(true, secretSeed, SecretLen.LEN_BIT_128, content)
+        return work(true, secretSeed, content)
     }
 
-    /**
-     * 参数以及返回值都是纯字节
-     *
-     * @param secretSeed 密钥
-     * @param content    要解密的字节
-     * @return 解密之后的字节
-     */
     fun decrypt(secretSeed: ByteArray, content: ByteArray?): ByteArray? {
-        return work(false, secretSeed, SecretLen.LEN_BIT_128, content)
-    }
-
-    fun encrypt(secretSeed: ByteArray, secretLen: SecretLen, content: ByteArray?): ByteArray? {
-        return work(true, secretSeed, secretLen, content)
-    }
-
-    fun decrypt(secretSeed: ByteArray, secretLen: SecretLen, content: ByteArray?): ByteArray? {
-        return work(false, secretSeed, secretLen, content)
+        return work(false, secretSeed, content)
     }
 
     private fun work(
         isEncrypt: Boolean,
         secretSeed: ByteArray,
-        secretLen: SecretLen,
-        todoContent: ByteArray?
-    ): ByteArray? {
-        try {
-            val iSecretLen = convertSecretLen(secretLen)
-            val iSecrentLenByte = iSecretLen / 8
-            if (secretSeed.size == iSecrentLenByte) {
-                return work(isEncrypt, secretSeed, iSecretLen, todoContent)
-            } else if (secretSeed.size > iSecrentLenByte) {
-                Log.w("AES", "密钥种子最好使用英文,如果使用了中文,获取的字节长度可能与期望不符.当种子长度大于'secretLen'时会截取!")
-                val _secretSeedBytes = ByteArray(iSecrentLenByte)
-                System.arraycopy(secretSeed, 0, _secretSeedBytes, 0, iSecrentLenByte)
-                return work(isEncrypt, _secretSeedBytes, iSecretLen, todoContent)
-            } else if (secretSeed.size < iSecrentLenByte) {
-                Log.e("AES", "密钥种子长度应该为128,192或者256位!")
-                return null
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return null
-    }
-
-    /**
-     * @param isEncrypt   是否为加密模式
-     * @param secretSeed  密钥种子,密钥是根据密钥种子和密钥长度生成的
-     * @param secretLen   密钥长度,只支持128,192,256三种(192,256两种有可能不支持)
-     * @param todoContent 需要被加密或者解密的字符串
-     * @return
-     */
-    private fun work(
-        isEncrypt: Boolean,
-        secretSeed: ByteArray,
-        secretLen: Int,
         todoContent: ByteArray?
     ): ByteArray? {
         var result: ByteArray? = null
@@ -153,7 +76,7 @@ object AESUtil {
             val cipher = Cipher.getInstance(CipherMode.AES_CBC_PKCS5Padding)
             cipher.init(
                 if (isEncrypt) Cipher.ENCRYPT_MODE else Cipher.DECRYPT_MODE, key, IvParameterSpec(
-                    getRawKey(secretLen, secretSeed)
+                    /*getRawKey(secretLen, secretSeed)*/secretSeed
                 )
             ) //IvParameterSpec增加加密算法的强度
             result = if (isEncrypt) {
@@ -177,39 +100,5 @@ object AESUtil {
             e.printStackTrace()
         }
         return result
-    }
-
-    /**
-     * @param secretLen  密钥长度,只支持128,192,256三种(192,256两种有可能不支持)
-     * @param secretSeed 密钥种子,密钥是根据密钥种子和密钥长度生成的
-     * @return
-     */
-    private fun getRawKey(secretLen: Int, secretSeed: ByteArray): ByteArray? {
-        var rawkey: ByteArray? = null
-        try {
-            val sr: SecureRandom
-            /*sr = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                SecureRandom.getInstance(Constant.Algorithm.SHA1PRNG, Constant.Provider.CRYPTO)
-            } else {
-                SecureRandom.getInstance(Constant.Algorithm.SHA1PRNG)
-            }*/
-            sr = SecureRandom.getInstance(Constant.Algorithm.SHA1PRNG)
-            sr.setSeed(secretSeed)
-            val kg = KeyGenerator.getInstance(Constant.Algorithm.AES)
-            kg.init(secretLen, sr)
-            val sk = kg.generateKey()
-            rawkey = sk.encoded
-        } catch (e: NoSuchAlgorithmException) {
-            e.printStackTrace()
-        } catch (e: NoSuchProviderException) {
-            e.printStackTrace()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return rawkey
-    }
-
-    enum class SecretLen {
-        LEN_BIT_128, LEN_BIT_192, LEN_BIT_256
     }
 }
