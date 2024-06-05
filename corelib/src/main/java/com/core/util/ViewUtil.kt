86b7540a93a5c1828abe93647c677224a266d6de
xyz <xyz@gmail.com>
2024-06-05 15:20:50 +0800

    1

---
+++ corelib/src/main/java/com/core/util/ViewUtil.kt
@@ -0,0 +1,83 @@
+package com.core.util
+
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.TextUtils
+import android.text.method.LinkMovementMethod
+import android.text.style.AbsoluteSizeSpan
+import android.text.style.ClickableSpan
+import android.text.style.ForegroundColorSpan
+import android.view.View
+import android.widget.TextView
+
+object ViewUtil {
+    fun setTargetTextsCustom(
+        textView: TextView,
+        fullText: String,
+        targetText1: String,
+        targetTextColor1: Int,
+        targetTextSize1: Float,
+        targetText2: String,
+        targetTextColor2: Int,
+        targetTextSize2: Float,
+        bgColor:Int,
+        targetText1Click: View.OnClickListener? = null,
+        targetText2Click: View.OnClickListener? = null
+    ) {
+        if (TextUtils.isEmpty(fullText)) {
+            return
+        }
+        val ss = SpannableString(fullText)
+        if (!TextUtils.isEmpty(targetText1)) {
+            val i = fullText.indexOf(targetText1)
+            if (i != -1) {
+                val colorSpan = ForegroundColorSpan(targetTextColor1)
+                val end = i + targetText1.length
+                ss.setSpan(colorSpan, i, end, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+                val sizeSpan = AbsoluteSizeSpan(targetTextSize1.toInt())
+                ss.setSpan(sizeSpan, i, end, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+                targetText1Click?.let {
+                    val clickSpan = object : ClickableSpan() {
+                        override fun onClick(widget: View) {
+                            it.onClick(widget)
+                        }
+
+                        override fun updateDrawState(ds: TextPaint) {
+                            ds.bgColor = bgColor
+                            ds.isUnderlineText = false
+                        }
+                    }
+                    ss.setSpan(clickSpan, i, end, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+                }
+            }
+        }
+        if (!TextUtils.isEmpty(targetText2)) {
+            val i = fullText.indexOf(targetText2)
+            if (i != -1) {
+                val colorSpan = ForegroundColorSpan(targetTextColor2)
+                val end = i + targetText2.length
+                ss.setSpan(colorSpan, i, end, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+                val sizeSpan = AbsoluteSizeSpan(targetTextSize2.toInt())
+                ss.setSpan(sizeSpan, i, end, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+                targetText2Click?.let {
+                    val clickSpan = object : ClickableSpan() {
+                        override fun onClick(widget: View) {
+                            it.onClick(widget)
+                        }
+
+                        override fun updateDrawState(ds: TextPaint) {
+                            ds.bgColor = bgColor
+                            ds.isUnderlineText = false
+                        }
+                    }
+                    ss.setSpan(clickSpan, i, end, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+                }
+            }
+        }
+        if (targetText1Click != null || targetText2Click != null) {
+            textView.movementMethod = LinkMovementMethod.getInstance()
+        }
+        textView.text = ss
+    }
+}
